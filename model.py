e61c9a152719ca23608ff42b268bd76461e1b829
Martinas-99 <martina.messinetti@gmail.com>
2024-07-02 09:22:53 +0200

    fix: fix random_word print only word and not date + fix get_last_n to give logical meaning

--- model.py
+++ model.py
@@ -107,16 +107,16 @@ class Dictionary:
     def get_all_dates(self) -> list[str]:
         return list(self.dictionary.keys())
     
-    def get_random_word(self) -> Tuple[str, Word]:
+    def get_random_word(self) -> list[Word]:
         random_date = random.choice(self.get_all_dates())
         random_word = random.choice(self.dictionary[random_date])
-        return (random_date, random_word)
+        return (random_word)
     
     def get_last_n_dates_words(self, n:int) -> list[Word]:
         all_dates = self.get_all_dates()
         all_dates.sort(reverse=True)
         result = []
-        for date in all_dates[:n]:
+        for date in all_dates[:n-1]:
             result += self.get_date_words(date)
             
         return result
